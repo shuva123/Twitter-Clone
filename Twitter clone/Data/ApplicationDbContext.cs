b468032f6fa70dc92ea1bdc4d3e47e4e344e4020
shuva123 <33460198+shuva123@users.noreply.github.com>
2019-07-11 16:48:59 +0530

    Add files via upload

---
+++ Twitter clone/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Jetweet.Models;
+
+namespace Jetweet.Data {
+  public class ApplicationDbContext : IdentityDbContext {
+    public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base (options) { }
+
+    public DbSet<Tweet> Tweet { get; set; }
+  }
+}
